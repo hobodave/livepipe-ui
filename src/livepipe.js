1f9b59c228f0aff6a4c36848f92bfc9308d46ea3
David Abdemoulaie <github@hobodave.com>
2008-09-29 16:10:23 -0500

    Fixes the "nonsecure items" errors in IE6, see ticket #9 in livepipe lighthouse

--- src/livepipe.js
+++ src/livepipe.js
@@ -316,7 +316,7 @@ var IframeShim = Class.create({
 	initialize: function() {
 		this.element = new Element('iframe',{
 			style: 'position:absolute;filter:progid:DXImageTransform.Microsoft.Alpha(opacity=0);display:none',
-			src: 'javascript:void(0);',
+			src: 'javascript:false;document.write("");',
 			frameborder: 0 
 		});
 		$(document.body).insert(this.element);
